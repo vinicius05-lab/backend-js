d9077631ee650e4fa14619ded643426553975908
vinicius05-lab <81199932+vinicius05-lab@users.noreply.github.com>
2025-01-14 08:38:55 -0300

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -6,8 +6,8 @@ categoryController = new CategoryController();
 
 router.post('/categories', categoryController.create);
 router.get('/categories', categoryController.getAll);
-router.get('/categories/id', categoryController.getById);
-router.put('/categories/id', categoryController.update);
-router.delete('/categories/id', categoryController.delete);
+router.get('/categories/:id', categoryController.getById);
+router.put('/categories/:id', categoryController.update);
+router.delete('/categories/:id', categoryController.delete);
 
 module.exports = router;
